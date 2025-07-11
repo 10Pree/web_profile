72c4df251dcb2175498f83107f34d5d00ea2d856
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-07-11 17:21:44 +0700

    feat(ui) use swiper

--- frontend/src/js/aos.js
+++ frontend/src/js/aos.js
@@ -1,3 +1,5 @@
-import Aos from "aos";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-Aos.init();
+AOS.init();
+
---
+++ frontend/src/js/swiper.js
@@ -0,0 +1,18 @@
+// import Swiper พร้อม bundle ทั้งหมด (รวม pagination, navigation ฯลฯ)
+import Swiper from 'swiper/bundle';
+
+// import CSS
+import 'swiper/css/bundle';
+
+// เริ่มต้นใช้งาน Swiper
+var swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
